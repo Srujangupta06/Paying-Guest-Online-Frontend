1779ade18582626670bff0dfb919c6ccc8e77648
Bharath4242 <140201328+Bharath4242@users.noreply.github.com>
2025-03-06 12:07:47 +0530

    first commit

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -2,8 +2,113 @@ const About = () => {
   return (
     <div>
       {/*You can remove this code and add your own content */}
-      <center>
-        <h1 className="text-3xl font-semibold">About</h1>
+      <center className="p-10 text-center bg-gray-50">
+          <div className="mt-6 w-full md:w-screen flex flex-col md:flex-row justify-around items-center gap-6">
+            <div className="w-full lg:2/3 mx-auto">
+              <h1 className="text-xl lg:text-2xl xl:text-4xl font-bold"> Your Trusted Partner for Hassle-Free Hostel & PG Bookings!</h1>
+              <p className="text-sm lg:text-base xl:text-2xl font-medium mt-5">We simplify your search for the perfect stay 
+                – seamless bookings, secure payments & verified listings.</p>
+              <div className="mt-5 flex flex-wrap justify-center gap-6">
+                <button className="bg-black text-white w-28 md:w-40 p-2 hover:shadow-2xl transition duration-500 rounded text-xs 
+                hover:scale-110 md:text-base cursor-pointer">
+                  Find Hostel</button>
+                <button className="bg-white text-black w-28 md:w-40 p-1 hover:shadow-2xl transition duration-500 rounded border-2
+                hover:scale-110 text-xs md:text-base cursor-pointer">List Your Hostel</button>
+              </div>
+            </div>
+            <div className="w-full md:w-4/6 ">
+              <img src="https://content.jdmagicbox.com/comp/def_content/paying_guest_accommodations/default-paying-guest-accommodations-5-250.jpg" 
+              className="shadow-2xl w-full md:w-2/3 mt-8 lg:mt-0 items-center rounded-full" />
+            </div>
+          </div>
+
+          <div className="mt-18 flex flex-col md:flex-row text-justify md:text-left md:items-center p-6">
+            <div className="min-w-4/12">
+              <h1 className="text-xl lg:text-2xl xl:text-4xl font-bold">
+                Trust<br/> Your Booking Partner</h1>
+            </div>
+            <div className="mx-auto md:text-center">
+              <h1 className="text-sm md:text-base lg:text-xl font-light mt-5">At Paying Guest Online, we streamline your hostel and 
+                PG bookings with ease and security. Our platform connects you with verified listings, ensuring a worry-free experience.
+              </h1>
+              <div className="mt-8 flex flex-wrap justify-center gap-6">
+                <button className="bg-black text-white w-28 md:w-40 p-2 rounded text-xs md:text-base cursor-pointer hover:shadow-2xl 
+                hover:scale-110 transition duration-500">
+                  Learn More</button>
+                <button className="bg-white text-black w-28 md:w-40 p-2 border-2 rounded text-xs md:text-base cursor-pointer hover:shadow-2xl 
+                hover:scale-110 transition duration-500">
+                  Sign Up</button>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="mt-14 text-start p-6">
+            <div className="w-full">
+              <h1 className="text-xl lg:text-4xl font-bold"><span className="text-base font-medium">Benefits</span><br/> 
+              Why Choose Us for Your Booking Needs?</h1>
+              <p className="text-sm md:text-base lg:text-xl font-light pt-4">We offer a range of features designed to enhance your booking experience. From verified 
+              listings to secure payments, we prioritize your comfort and safety.</p>
+            </div>
+          </div>
+
+
+          <div className="mt-18 text-left p-2">
+            <div className="flex flex-wrap justify-center xl:justify-between items-center gap-12">
+              <div className="w-80 shadow flex flex-col justify-center items-center p-6 hover:shadow-neutral-950">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6P3lonz51VlLdC6HK9_LuGiYU-FI8nT6fw&s" 
+                className="w-24 rounded-t-full"></img>
+                <h1 className="text-xl font-bold pt-4">Key Benefits for Tenants</h1>
+                <p className="text-xs font-medium pt-4">Enjoy verified accommodations, secure payments, and 
+                flexible stay options.</p>
+              </div>
+              <div className="w-80 shadow flex flex-col justify-center items-center p-6 hover:shadow-neutral-950">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6P3lonz51VlLdC6HK9_LuGiYU-FI8nT6fw&s" 
+                className="w-24 rounded-t-full"></img>
+                <h1 className="text-xl font-bold pt-4">Key Benefits for Hostel Owners</h1>
+                <p className="text-xs font-medium pt-4">Boost your visibility and streamline property 
+                management effortlessly.</p>
+              </div>
+              <div className="w-80 shadow flex flex-col justify-center items-center p-6 hover:shadow-neutral-950">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6P3lonz51VlLdC6HK9_LuGiYU-FI8nT6fw&s" 
+                className="w-24 rounded-t-full"></img>
+                <h1 className="text-xl font-bold pt-4">Our Commitment to Quality 
+                and Trust</h1>
+                <p className="text-xs font-medium pt-4">We ensure transparency and reliability in every booking.</p>
+              </div>
+            </div>
+            <div className="mt-5 text-center lg:text-left">
+                <button className="border-1 text-black w-40 p-2 rounded text-xs md:text-base cursor-pointer hover:shadow-2xl hover:scale-110
+                transiton duration-500">
+                  Learn More</button>
+                <button className="text-black w-40 p-2 rounded text-xs md:text-base cursor-pointer hover:shadow-2xl hover:scale-110
+                transiton duration-500">
+                  Sign Up</button>
+              </div>
+          </div>
+
+
+            <p className="mt-14 text-lg max-w-3xl mx-auto">
+              At PGO, we simplify your search for the perfect stay with seamless bookings, secure payments, and verified listings. Our mission is to provide a trustworthy and efficient platform for tenants and hostel owners alike.
+            </p>
+
+          <div className="mt-6 flex flex-wrap justify-center gap-6">
+            <div className="bg-white p-6 shadow rounded w-80">
+              <h3 className="font-semibold">Our Mission</h3>
+              <p>Our mission is to simplify the process of finding paying guest (PG) accommodations by providing a reliable and user-friendly online platform. We aim to bridge the gap between PG owners and tenants by offering a seamless experience that ensures safety, affordability, and convenience. Through technology and innovation, we strive to eliminate the challenges of searching for quality accommodations and create a hassle-free solution for students and professionals looking for a comfortable place to stay.
+              </p>
+            </div>
+            <div className="bg-white p-6 shadow rounded w-80">
+              <h3 className="font-semibold">Our Vision</h3>
+              <p>Our vision is to become the most trusted and preferred online platform for PG accommodations, making the search for a home away from home easier than ever. We aspire to set new standards in the industry by ensuring transparency, security, and a smooth renting process. By continuously improving our services and embracing new technologies, we envision a future where anyone can find their ideal PG accommodation with just a few clicks, without stress or uncertainty.
+              </p>
+            </div>
+            <div className="bg-white p-6 shadow rounded w-80">
+              <h3 className="font-semibold">Our Values</h3>
+              <p>We are committed to trust and transparency, ensuring verified listings and honest communication. Our focus on convenience and accessibility makes finding and booking PG accommodations effortless. We prioritize affordability and quality, offering the best value without compromising comfort and safety. A customer-centric approach drives us to provide excellent support and personalized recommendations. Lastly, we embrace innovation and continuous improvement, using technology to enhance the overall experience. These values shape our mission to make PG accommodation searches simple, secure, and hassle-free.
+              </p>
+            </div>
+          </div>
       </center>
     </div>
   );
